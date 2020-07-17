bb4168e27af337c22f2262a4af04e7dd9720153f
qianyu <1465517072@qq.com>
2020-07-17 11:31:38 +0800

    测试上传路径

--- src/main/java/com/lening/ceshi.java
+++ src/main/java/com/lening/ceshi.java
@@ -7,5 +7,9 @@ package com.lening;
 public class ceshi {
     public static void main(String[] args) {
         System.out.println("你好啊");
+        System.out.println("你好啊");
+        System.out.println("你好啊");
+        System.out.println("你好啊");
+        System.out.println("你好啊");
     }
 }
